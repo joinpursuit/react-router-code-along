6b067e8b14f59086bb8ebae05ba38334d9f9edee
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-05-14 11:18:27 -0400

    class activity

--- src/components/products/product.js
+++ src/components/products/product.js
@@ -1,9 +1,18 @@
+import { useParams, useNavigate, Navigate } from "react-router-dom";
+
 export default function Product({products}) {
-    let id
+    
+    const navigate = useNavigate();
     const mockBuyNow = () => {
-        alert('You pressed Buy Now! Congrats! Our sophisticated system is already charging your card and sending you your purchase. Let\'s go back to the home page!')
-    }
-    const  [product] = products.filter( product => product.id === id)
+        alert(
+            "You pressed Buy Now! Congrats! Our sophisticated system is already charging your card and sending you your purchase. Let's go back to the home page!"
+        );
+        navigate("/");
+    };
+
+    let { id } = useParams();
+    const [product] = products.filter((product) => product.id === id);
+    
         if (product) {return (
             <section>
             <h5>{product.name}</h5>
@@ -11,7 +20,7 @@ export default function Product({products}) {
             <p>{product.type}</p>
             <p>{product.style}</p>
             <p>${product.price}</p>
-            <button>Buy Now!</button>
+            <button onClick={mockBuyNow}>Buy Now!</button>
         </section>
         )}
         return (<h1>No products found</h1>)     
